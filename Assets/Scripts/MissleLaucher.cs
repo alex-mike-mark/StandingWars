cae9aa8615dfe842e074fb48769301a75f2ee6e9
KeroseneCity <alex.mike.mark>
2017-11-17 20:58:00 -0600

    Missle spawns every 100 frames.

---
+++ Assets/Scripts/MissleLaucher.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissleLaucher : MonoBehaviour {
+	public Transform missle;
+	int i;
+
+	// Use this for initialization
+	void Start () {
+		i = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (i % 100 == 0) {
+			Instantiate(missle, new Vector3(0,i/100,0), Quaternion.identity);
+		}
+		i++;
+	}
+}
