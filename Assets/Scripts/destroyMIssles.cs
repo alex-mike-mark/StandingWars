d9d254296d59b2767cfd06c553c832c938310af1
mardelmaduro <mardel.maduro@gmail.com>
2017-11-18 21:24:44 -0600

    Update_MM_2
    Fixed Missle Paths
    Added in Health Bar
    Added in missle to player2 collissions
    etc.

--- Assets/Scripts/destroyMIssles.cs
+++ Assets/Scripts/destroyMIssles.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class destroyMIssles : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll) 
 	{
 		coll.gameObject.SetActive (false);
 		Destroy(coll.gameObject);
+
+		if (this.gameObject.tag == "Player2") 
+		{
+			this.GetComponent <HealthScript>().health -= 10f;
+			iTween.ShakePosition (this.gameObject , this.transform.right * 0.05f , 0.75f );
+		}
+
 	}
 }
